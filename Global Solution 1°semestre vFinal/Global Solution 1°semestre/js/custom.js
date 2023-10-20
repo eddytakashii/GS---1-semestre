5f94401c281b362171e642999c8b7d0b4bfa004b
eddytakashii <nakauchi.takashi@gmail.com>
2023-10-20 18:11:40 -0300

    first commit

---
+++ Global Solution 1°semestre vFinal/Global Solution 1°semestre/js/custom.js
@@ -0,0 +1,15 @@
+
+function carConteudo(){
+ document.getElementById('conteudo').innerHTML= " <p>Estação chuvosa: A cana-de-açúcar requer um suprimento adequado de água durante seu estágio inicial de crescimento. Portanto, o plantio é geralmente realizado antes ou durante a estação chuvosa, quando a quantidade de chuva é suficiente para proporcionar umidade adequada no solo.</p>"
+}
+
+function carConteudo01(){
+    document.getElementById('conteudo01').innerHTML= " <p>A melhor época para plantar milho pode variar dependendo da região e das condições climáticas locais. No entanto, geralmente, a época ideal para o plantio do milho é na primavera, quando a temperatura do solo atinge cerca de 10 a 12 graus Celsius. O milho é uma cultura que requer temperaturas mais altas para germinar e se desenvolver adequadamente.<p/>"
+}
+
+   function carConteudo02(){
+    document.getElementById('conteudo02').innerHTML= " <p>A alface é uma cultura de clima temperado e pode ser plantada em diferentes épocas do ano, dependendo da variedade e das condições climáticas locais.<p/>"   
+}
+function carConteudo03(){
+    document.getElementById('conteudo03').innerHTML= " <p>Conforme a Embrapa, os meses mais indicados para plantar arroz no Brasil, de acordo com algumas regiões, são: Para o arroz de sequeiro: de outubro a dezembro, podendo se estender até janeiro, nas localidades das regiões Norte e Nordeste do país.<p/>"   
+}
